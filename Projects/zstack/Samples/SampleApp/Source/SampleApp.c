b959b0b57f68f01dc0a1cf6c8c65850e6139c629
ZhengPengqiao <157510312@qq.com>
2016-10-05 16:02:27 +0800

    基础实验，只是将Z-stack解压后的工程

---
+++ Projects/zstack/Samples/SampleApp/Source/SampleApp.c
@@ -0,0 +1,86 @@
+/*****************************************************************************
+*  文件名称  ： 最简实验例程1
+*    作者    ： 郑朋桥
+*    时间    ： 2016/8/18
+******************************************************************************/
+#include "OSAL.h"
+#include "ZDApp.h"
+#include "SampleApp.h"
+
+/* HAL */
+#include "hal_led.h"
+#include "hal_key.h"
+#include "MT_UART.h"
+
+
+
+//按键事件处理函数声明
+void SampleApp_HandleKeys( uint8 shift, uint8 keys );
+
+
+
+/******************************************
+*              
+*     函数名称：SampleApp_Init
+*     函数功能：应用层初始化
+*
+*******************************************/
+void SampleApp_Init( uint8 task_id )
+{ 
+    RegisterForKeys( task_id ); // 登记所有的按键事件
+}
+
+
+/******************************************
+*              
+*     函数名称：SampleApp_ProcessEvent
+*     函数功能：应用层任务处理函数
+*
+*******************************************/
+uint16 SampleApp_ProcessEvent( uint8 task_id, uint16 events )
+{
+      afIncomingMSGPacket_t *MSGpkt;
+      (void)task_id;  // Intentionally unreferenced parameter
+
+      if ( events & SYS_EVENT_MSG ) //接收系统消息再进行判断
+      {
+            MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( task_id );
+            while ( MSGpkt )
+            {
+              switch ( MSGpkt->hdr.event )
+              {        
+                   case KEY_CHANGE://按键事件
+                    SampleApp_HandleKeys( ((keyChange_t *)MSGpkt)->state, ((keyChange_t *)MSGpkt)->keys );
+                   break;
+              }
+
+              // Release the memory 
+              osal_msg_deallocate( (uint8 *)MSGpkt );
+
+              // Next - if one is available 
+              MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( task_id );
+            }
+
+            // return unprocessed events 
+            return (events ^ SYS_EVENT_MSG);
+      }
+      
+      return 0;
+}
+
+
+/******************************************
+*              
+*     函数名称：SampleApp_HandleKeys
+*     函数功能：按键事件处理函数
+*
+*******************************************/
+void SampleApp_HandleKeys( uint8 shift, uint8 keys ) 
+{
+      (void)shift;  // Intentionally unreferenced parameter
+      
+      if ( keys & HAL_KEY_SW_6 ) //S1
+      {
+
+      }
+}
